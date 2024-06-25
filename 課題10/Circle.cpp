26e306ac791bc2db40b71c17806ca89ade16b0e7
悠斗 豊川 <230079@jc-21.jp>
2024-06-25 11:49:28 +0900

    課題9と10

---
+++ 課題10/Circle.cpp
@@ -0,0 +1,11 @@
+#include"Circle.h"
+
+Circle::Circle()
+{
+	PI = 3.14;
+}
+
+float Circle::Menseki(float r)
+{
+	return r * r * PI;
+}
---
+++ 課題10/Circle.h
@@ -0,0 +1,9 @@
+class Circle
+{
+	//メンバ変数
+	float PI; //円周率
+	//メンバ関数
+public:
+	Circle(); //コンストラクタ
+	float Menseki(float r); //面積を求める関数
+};

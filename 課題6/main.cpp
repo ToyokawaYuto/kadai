1a97184cc7d62874480b122e1401dac5c8ac3df2
悠斗 豊川 <230079@jc-21.jp>
2024-06-25 12:51:18 +0900

    課題12と13

--- 課題6/main.cpp
+++ 課題6/main.cpp
@@ -1,12 +1,29 @@
-#include "Calclation.h"
-
+#include "Calculation.h"
+//プロトタイプ宣言
+void SetX(float a, float b);
+void SetY(float a, float b);
+//グローバル変数
+Calculation x, y;
+//メイン関数
 int main()
 {
-	float a = 42.195f, b = 3.14f;
-
-	Calclation x;
-
+	//インスタンスＸの処理
+	SetX(5.0, 10.0);
+	x.Disp();
+	//インスタンスＹの処理
+	SetY(8.0, 3.0);
+	y.Disp();
+}
+//インスタンスＸのアクセス関数を呼ぶ
+void SetX(float a, float b)
+{
 	x.SetA(a);
 	x.SetB(b);
-	x.Disp();
+}
+//インスタンスＹのアクセス関数を呼ぶ
+void SetY(float a, float b)
+{
+	y.SetA(a);
+	y.SetB(b);
+}
 }
